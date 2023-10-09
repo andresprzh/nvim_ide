1917f81818786bd3a5acf4763ab8e24a94340bc7
jaime andres perez <andresprzh@gmail.com>
2023-10-09 03:11:55 -0500

    main: change black for autopep8 in the python formatting in lsp

--- lua/modules/lsp.lua
+++ lua/modules/lsp.lua
@@ -27,7 +27,7 @@ require("mason-null-ls").setup({
 local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.autopep8,
   },
 })
 
